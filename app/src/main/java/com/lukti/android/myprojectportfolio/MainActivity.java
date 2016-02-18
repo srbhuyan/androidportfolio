57fd600d637ff909ed3c2c1c939f832fefbb389c
Soumya Ranjan Bhuyan <soumya.tiklu@gmail.com>
2016-02-18 00:32:56 -0700

    Initial commit

---
+++ app/src/main/java/com/lukti/android/myprojectportfolio/MainActivity.java
@@ -0,0 +1,42 @@
+package com.lukti.android.myprojectportfolio;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void showMessage(View view){
+        Toast.makeText(getApplicationContext(), "This will launch the " + ((Button)view).getText() + " app.", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
